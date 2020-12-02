a03430062d22dec631ac89da48ef66c1ae70c510
Tedford <dev@tedfordjohnson.com>
2020-12-02 14:00:25 -0500

    Day 2 - Part 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -6,7 +6,8 @@ struct Credential {
     max: usize,
     token: char,
     password: String,
-    valid: bool,
+    policy1: bool,
+    policy2: bool,
 }
 
 fn parse_record(input: &str) -> Credential {
@@ -14,50 +15,54 @@ fn parse_record(input: &str) -> Credential {
     let range: Vec<&str> = iter.next().unwrap().split("-").collect();
     let min = range[0].parse::<usize>().unwrap();
     let max = range[1].parse::<usize>().unwrap();
-
     let token = iter.next().unwrap().chars().next().unwrap();
-    let password = iter.next().unwrap();
-
+    let password = iter.next().unwrap().to_string();
     let count = password.chars().filter(|c| *c == token).count();
+
+    let mut matches: i32 = 0;
+    if password.chars().nth(min - 1).unwrap() == token {
+        matches = matches + 1;
+    }
+    if password.chars().nth(max - 1).unwrap() == token {
+        matches = matches + 1;
+    }
 
     Credential {
         min: min,
         max: max,
         token: token,
-        password: password.to_string(),
-        valid: count >= min && count <= max,
+        password: password,
+        policy1: count >= min && count <= max,
+        policy2: matches == 1,
     }
 }
 
 fn load_credentials(path: &str) -> Vec<Credential> {
-    let credentials: Vec<Credential> = fs::read_to_string(path)
+    fs::read_to_string(path)
         .expect("Unable to load the data file")
         .lines()
         .map(|x| parse_record(x))
-        .collect();
-    credentials
+        .collect()
+}
+
+fn print_report(path: &str) {
+    let parts: Vec<&str> = path.split("\\").collect();
+    let filename = parts[parts.len() - 1];
+
+    println!("{}", filename);
+    let credentials = load_credentials(path);
+    println!(
+        "\t{0} of {2} passwords are valid with Policy 1\n\t{1} of {2} are valid with Policy 2",
+        credentials.iter().filter(|c| c.policy1).count(),
+        credentials.iter().filter(|c| c.policy2).count(),
+        credentials.len()
+    )
 }
 
 fn main() {
     println!("Day 2");
     println!("=============");
-    println!("Part1");
-    println!("=======");
 
-    println!("day2.example.txt");
-    let credentials =
-        load_credentials("c:\\projects\\github\\advent-of-code-2020\\data\\day2.example.txt");
-    println!(
-        "\t{} of {} passwords are valid",
-        credentials.iter().filter(|c| c.valid).count(),
-        credentials.len()
-    );
-
-    println!("day2.txt");
-    let credentials = load_credentials("c:\\projects\\github\\advent-of-code-2020\\data\\day2.txt");
-    println!(
-        "\t{} of {} passwords are valid",
-        credentials.iter().filter(|c| c.valid).count(),
-        credentials.len()
-    );
+    print_report("c:\\projects\\github\\advent-of-code-2020\\data\\day2.example.txt");
+    print_report("c:\\projects\\github\\advent-of-code-2020\\data\\day2.txt");
 }
