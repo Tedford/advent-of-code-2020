238d039e547e1a377c925e7f23ef217c9ab71d09
Tedford <dev@tedfordjohnson.com>
2020-12-03 16:03:33 -0500

    Day 3 - Part 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,23 +1,16 @@
 use std::fmt;
 use std::fs;
 
+#[derive(Copy, Clone)]
 struct Movement {
     x: usize,
     y: usize,
 }
 
-fn convert(line: &str) -> Vec<bool> {
-    line.chars()
-        .map(|c| if c == '.' { false } else { true })
-        .collect()
-}
-
-fn load_map(path: &str) -> Vec<Vec<bool>> {
-    fs::read_to_string(path)
-        .expect("Unable to load the data file")
-        .lines()
-        .map(|x| convert(x))
-        .collect()
+#[derive(Clone)]
+struct Context {
+    filename: String,
+    map: Vec<Vec<bool>>,
 }
 
 impl fmt::Display for Movement {
@@ -26,35 +19,85 @@ impl fmt::Display for Movement {
     }
 }
 
-fn get_filename(path: &str) -> &str {
+fn load_map(path: &str) -> Context {
     let parts: Vec<&str> = path.split("\\").collect();
-    parts[parts.len() - 1]
+
+    let map = fs::read_to_string(path)
+        .expect("Unable to load the data file")
+        .lines()
+        .map(|x| {
+            x.chars()
+                .map(|c| if c == '.' { false } else { true })
+                .collect()
+        })
+        .collect();
+
+    Context {
+        map,
+        filename: parts[parts.len() - 1].to_string(),
+    }
 }
 
-fn map_descent(path: &str, movement: Movement) {
-    println!("Data File {0}, Movement {1}", get_filename(path), movement);
+fn descend(map: &Vec<Vec<bool>>, y: usize) -> Option<Vec<bool>> {
+    if map.len() - 1 < y {
+        None
+    } else {
+        Some(map[y].clone())
+    }
+}
 
-    let encountered = load_map(path).iter().fold((0, 0), |acc, row| 
-        // println!("x = {0}, Tree = {1}",acc.0, row[acc.0]);
-        (
-            (acc.0 + movement.x) % row.iter().len(),
-            acc.1 + (if row[acc.0] { 1 } else { 0 }),
-        )
-    );
+fn map_descent(map: &Vec<Vec<bool>>, movement: &Movement) -> i64 {
+    let mut y = 0;
+    let mut x = 0;
+    let mut encountered = 0;
 
-    println!("Encountered {} trees", encountered.1);
+    while let Some(row) = descend(map, y) {
+        encountered = encountered + if row[x] { 1 } else { 0 };
+        y = y + movement.y;
+        x = (x + movement.x) % row.len();
+    }
+
+    encountered
 }
 
 fn main() {
     println!("Day 3");
     println!("=============");
 
-    map_descent(
-        "c:\\projects\\github\\advent-of-code-2020\\data\\day3.example.txt",
+    let contexts = [
+        load_map("c:\\projects\\github\\advent-of-code-2020\\data\\day3.example.txt"),
+        load_map("c:\\projects\\github\\advent-of-code-2020\\data\\day3.txt"),
+    ];
+
+    println!("Part 1");
+    for context in contexts.iter() {
+        println!(
+            "\t{}\t{}",
+            map_descent(&context.map, &Movement { x: 3, y: 1 },),
+            context.filename
+        );
+    }
+
+    println!("Part 2");
+    let routes = [
+        Movement { x: 1, y: 1 },
         Movement { x: 3, y: 1 },
-    );
-    map_descent(
-        "c:\\projects\\github\\advent-of-code-2020\\data\\day3.txt",
-        Movement { x: 3, y: 1 },
-    );
+        Movement { x: 5, y: 1 },
+        Movement { x: 7, y: 1 },
+        Movement { x: 1, y: 2 },
+    ];
+
+    let costs = contexts.iter().map(|c| {
+        (
+            routes
+                .iter()
+                .map(|r| map_descent(&c.map, r))
+                .fold(1, |acc, encountered| acc * encountered),
+            c.filename.to_owned(),
+        )
+    });
+
+    for cost in costs {
+        println!("\t{}\t{}", cost.0, cost.1);
+    }
 }
