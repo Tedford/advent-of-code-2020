6664a2e1c8771055bb14bc5b02d493b0e7ddbf7b
Tedford <dev@tedfordjohnson.com>
2020-12-08 11:45:00 -0500

    Day 7 - Part 2

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,28 +1,6 @@
 use std::collections::HashMap;
 use std::collections::HashSet;
-use std::fmt;
 use std::fs;
-
-#[derive(Debug)]
-struct Rule {
-    color: String,
-    contents: HashMap<String, i32>,
-}
-
-impl fmt::Display for Rule {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            f,
-            "{0}: {{ {1} }}",
-            self.color,
-            self.contents
-                .iter()
-                .map(|(k, v)| format!("{}:{}", k, v))
-                .collect::<Vec<String>>()
-                .join(", ")
-        )
-    }
-}
 
 fn parse_contents(s: &str) -> HashMap<String, i32> {
     if s.starts_with("no") {
@@ -35,21 +13,18 @@ fn parse_contents(s: &str) -> HashMap<String, i32> {
                 let name = format!("{} {}", parts.next().unwrap(), parts.next().unwrap());
                 (name, count)
             })
-            .collect::<HashMap<String, i32>>()
+            .collect()
     }
 }
 
-fn decompose(s: &str) -> Rule {
+fn decompose(s: &str) -> (String, HashMap<String, i32>) {
     let mut parts = s.split("bags contain");
     let color = parts.next().unwrap().trim().to_string();
 
-    Rule {
-        color: color,
-        contents: parse_contents(parts.next().unwrap().trim()),
-    }
+    (color, parse_contents(parts.next().unwrap().trim()))
 }
 
-fn load(path: &str) -> Vec<Rule> {
+fn load(path: &str) -> HashMap<String, HashMap<String, i32>> {
     fs::read_to_string(path)
         .expect(path)
         .lines()
@@ -57,15 +32,16 @@ fn load(path: &str) -> Vec<Rule> {
         .collect()
 }
 
-fn find_parent(rules: &Vec<Rule>, color: &str) -> Vec<String> {
+fn find_parent(rules: &HashMap<String, HashMap<String, i32>>, color: &str) -> Vec<String> {
     rules
         .iter()
-        .filter(|r| r.contents.contains_key(color))
-        .map(|r| r.color.clone())
+        .filter(|(_, v)| v.contains_key(color))
+        .map(|(k, _)| k.clone())
         .collect()
 }
 
-fn can_contain(rules: &Vec<Rule>, color: &str) -> i32 {
+// replace with fold
+fn can_contain(rules: &HashMap<String, HashMap<String, i32>>, color: &str) -> i32 {
     let mut inspectable = Some(find_parent(&rules, color));
 
     let mut candidates: HashSet<String> = HashSet::new();
@@ -89,14 +65,20 @@ fn can_contain(rules: &Vec<Rule>, color: &str) -> i32 {
     candidates.len() as i32
 }
 
+fn count_children(rules: &HashMap<String, HashMap<String, i32>>, color: &str) -> i32 {
+    let children = rules.get(color).unwrap();
+    children
+        .iter()
+        .map(|(color, count)| count + count * count_children(rules, color))
+        .sum::<i32>()
+}
+
 fn calibrate_part1() {
     println!("Calibration");
-    let inputs = [
-        (
-            r"c:\projects\github\advent-of-code-2020\data\day7.example.txt",
-            4,
-        ),
-    ];
+    let inputs = [(
+        r"c:\projects\github\advent-of-code-2020\data\day7.example.1.txt",
+        4,
+    )];
 
     for input in inputs.iter() {
         let rules = load(input.0);
@@ -113,34 +95,35 @@ fn calibrate_part1() {
     }
 }
 
+fn calibrate_part2() {
+    println!("Calibration");
+    let inputs = [(
+        r"c:\projects\github\advent-of-code-2020\data\day7.example.2.txt",
+        126,
+    )];
+
+    for input in inputs.iter() {
+        let rules = load(input.0);
+        let count = count_children(&rules, "shiny gold");
+        println!(
+            "{0} => {1} [{2}]",
+            input.0,
+            count,
+            match count == input.1 {
+                true => "SUCCESS",
+                _ => "FAILED",
+            }
+        );
+    }
+}
+
 fn main() {
     println!("Day 7");
     println!("=============");
+    let rules = load(r"c:\projects\github\advent-of-code-2020\data\day7.txt");
 
     calibrate_part1();
-
-    let rules = load(r"c:\projects\github\advent-of-code-2020\data\day7.txt");
-    println!("Part 1: {}", can_contain(&rules,"shiny gold"));
-
-    // let mut inspectable = Some(find_parent(&rules, "shiny gold"));
-
-    // let mut candidates: HashSet<String> = HashSet::new();
-
-    // while let Some(parents) = inspectable {
-    //     let mut next = Vec::<String>::new();
-
-    //     for parent in parents {
-    //         candidates.insert(parent.clone());
-    //         for grandparent in find_parent(&rules, &parent) {
-    //             next.push(grandparent);
-    //         }
-    //     }
-
-    //     match next.len() {
-    //         0 => inspectable = None,
-    //         _ => inspectable = Some(next),
-    //     }
-    // }
-
-    // println!("{} can hold {}", candidates.len(), "shiny gold");
+    println!("Part 1: {}", can_contain(&rules, "shiny gold"));
+    calibrate_part2();
+    println!("Part 2: {}", count_children(&rules, "shiny gold"));
 }
